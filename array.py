1746003192751836b2c039dd97a6407d114ac9fc
Aaron Lo <22aaronl@students.harker.org>
2022-10-29 11:53:42 -0500

    weather data

--- array.py
+++ array.py
@@ -1,15 +1,24 @@
 import requests
+import math
+from weather import Weather
 
 #model for a solar cell array
 class Array:
-    def __init__(self):
+    def __init__(self, weather):
         self.num_c60 = 300
         self.num_e60 = 90
+        self.weather = weather
+        
+    def calculateIrradiance(self):
+        angle_to_sun = self.weather.get_angle_of_sun()
+        intensity_from_sun = self.weather.get_intensity()
+        return math.cos(angle_to_sun) * intensity_from_sun
+        
 
     def get_power(self):
         solarcell_c60 = SolarCell(solarcell_c60, 153.328, 0.225, 25, -0.00342) #unsure of the temperature_coefficients
         solarcell_e60 = SolarCell(solarcell_e60, 153.328, 0.237, 25, -0.00363)
-        return solarcell_c60.get_power_gen(1000) * self.num_c60 + solarcell_e60.get_power_gen(1000) * self.num_e60
+        return solarcell_c60.get_power_gen(self.calculateIrradiance()) * self.num_c60 + solarcell_e60.get_power_gen(self.calculateIrradiance()) * self.num_e60
     
     
     
@@ -40,3 +49,9 @@ class SolarCell:
     def get_power_gen(self, light_intensity):
         temperature_loss = (self.temperature - self.standard_temp) * self.temperature_coefficient
         return self.area * self.efficiency * light_intensity * (1 - temperature_loss)
+    
+    
+if __name__ == "___main__":
+    weather = Weather(0,0,0)
+    array = Array(weather)
+    print(array.get_power())
